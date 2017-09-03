46c54b5fcaef82832e1990e8bae5385d39eb0cf2
fkk1024 <15958119316@sina.cn>
2017-09-03 22:18:12 +0800

    调整地图遮罩bug

--- js/main.js
+++ js/main.js
@@ -90,86 +90,10 @@ function fixSizes() {
 }
 
 // 联系方式切换
-//$(()=>{
-//	$("#contact .social").on("click",".iconfont",function(){
-//		var index=$(this).index();
-//		var data=[];
-//		var C=$("#showContainer");//取容器               
-//		data[0]="<img src='img/我的微信.jpg' style='width:150px;'>";
-//		data[1]="<p>手机：15958119316</p>";
-//		data[2]="<p>GitHub项目地址:https://github.com/fkk1024</p>";
-//		// data[3]="<iframe src="map.html"></iframe>";
-//		console.log($(this).index());
-//		if (index===3) {
-//			C.html("<div id='map'></div>");
-//			let h='600';
-//			// let w=window.screen.width;
-//			var el=$("#map");
-//			el.css({
-//				width:'100%',
-//				height:h+'px'
-//			})
-//			// el.style.width=w+'px';
-//			// el.style.height=h+'px';
-//			// }
-//			// window.onload=a;
-//			var map = new AMap.Map('#map',{
-//			    zoom: 16,
-//			    scrollWheel:true,
-//			    center: [120.208732,30.166941]
-//			});
-//			var marker = new AMap.Marker({
-//			    position: [120.208732,30.166941],//marker所在的位置
-//			    map:map//创建时直接赋予map属性
-//			});
-//			marker.setMap(map);
-//			//监听地图的click事件，点击地图，允许鼠标滚轮缩小或放大地图
-//	                var clickEventListener = map.on('click', function() {
-//	                map.setStatus({scrollWheel:true});
-//	            });
-//	            //监听地图的mouseout事件，鼠标移出地图区域，鼠标滚轮缩小或放大地图功能取消
-//	            var mouseOutEventListener = map.on('mouseout',function(){
-//	            map.setStatus({scrollWheel:false});
-//	            });
-//			// 工具控件
-//			AMap.plugin(['AMap.ToolBar','AMap.MapType','AMap.Geolocation'],function(){
-//			    //创建并添加工具条控件
-//			    var toolBar = new AMap.ToolBar();
-//			    var Geolocation = new AMap.Geolocation();
-//			    var mapType = new AMap.MapType();
-//			    map.addControl(toolBar);
-//			    // map.addControl(mapType);
-//			    map.addControl(Geolocation);
-//			})
-//			 var info = new AMap.InfoWindow({
-//		        content: '<div class="title">现居地址：滨江区</div><div class="content">'+
-//		                '白马湖小区-白鹤苑<br/>',
-//		        offset:new AMap.Pixel(0,-28),
-//		        size:new AMap.Size(200,0)
-//		    })
-//		    info.open(map,marker.getPosition())
-//		    $('#contact').css({
-//				'paddingBottom':'10px',
-//				'textAlign': 'center',
-//			}).children().find('#showContainer').css({
-//				'marginTop':'20px',
-//				'height':'600px'
-//			})
-//		}else{
-//			if (C.html()!=='') {
-//				C.html('');
-//			}
-//			$('#showContainer').html(data[index]);
-//			$('#contact').css({
-//				'textAlign': 'center',
-//				'paddingBottom':'10px'
-//			}).children().find('#showContainer').css({
-//				'marginTop':'20px',
-//				'height':'220px'
-//			}).children().css({
-//				'height':'200px',
-//				'lineHeight':'200px'
-//			})
-//		}
-//	})
-//})
+$("#contact .social").on("click",".iconfont",function(){
+	var index=$(this).index();
+	console.log(index);
+	var C=$("#showContainer li");//取容器
+	console.log(C);
+	C.css('display','none').eq(index).css('display','block'); 
+})
